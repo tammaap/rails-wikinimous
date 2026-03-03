c1753b29566721f9e56c147a2ec7a09b371ef46c
Marta Aparicio <martaaparicio14@gmail.com>
2026-03-03 16:22:32 +0100

    rails wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# puts "Cleaning database..."
+# Article.destroy_all
+
+puts "Creating articles..."
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+  article.save!
+end
+puts "Finished!"
